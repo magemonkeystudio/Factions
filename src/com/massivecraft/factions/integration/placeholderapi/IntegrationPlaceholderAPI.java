4ce820d1cf18b39edbae969ad1d9e781977c3e64
Magnus Ulf <magnus@muj.dk>
2019-01-29 09:24:30 +0100

    Fix bug with PlaceholderAPI integration

--- src/com/massivecraft/factions/integration/placeholderapi/IntegrationPlaceholderAPI.java
+++ src/com/massivecraft/factions/integration/placeholderapi/IntegrationPlaceholderAPI.java
@@ -1,7 +1,6 @@
 package com.massivecraft.factions.integration.placeholderapi;
 
 import com.massivecraft.massivecore.Integration;
-import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -27,7 +26,7 @@ public class IntegrationPlaceholderAPI extends Integration
 	@Override
 	public void setIntegrationActiveInner(boolean active)
 	{
-		PlaceholderAPIPlugin.getInstance().getExpansionManager().registerExpansion(PlaceholderFactions.get());
+		PlaceholderFactions.get().register();
 	}
 
 	public static void ensureRegistered()
@@ -45,7 +44,7 @@ public class IntegrationPlaceholderAPI extends Integration
 		if (str.startsWith("/")) str = str.substring(1);
 
 		if (!str.startsWith("papi")) return;
-		Bukkit.getScheduler().runTaskLater(this.getPlugin(), IntegrationPlaceholderAPI::ensureRegistered, 20L);
+		Bukkit.getScheduler().runTaskLater(this.getPlugin(), IntegrationPlaceholderAPI::ensureRegistered, 10L);
 	}
 
 	
