94b424b8dc50d216cac31607baed2be0412948e3
Magnus Ulf <magnus@muj.dk>
2021-03-02 21:29:36 +0100

    Attempt fix #82

--- src/com/massivecraft/factions/cmd/CmdFactionsMotd.java
+++ src/com/massivecraft/factions/cmd/CmdFactionsMotd.java
@@ -42,8 +42,11 @@ public class CmdFactionsMotd extends FactionsCommand
 		// Args
 		String target = this.readArg();
 
-		target = target.trim();
-		target = Txt.parse(target);
+		if (target != null)
+		{
+			target = target.trim();
+			target = Txt.parse(target);
+		}
 
 		// Get Old
 		String old = msenderFaction.getMotd();
